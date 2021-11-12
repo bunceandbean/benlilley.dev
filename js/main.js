f54e0d2884e0d41028d278e38dd8501ad6956208
Ben Lilley <benlilley1025@gmail.com>
2021-11-11 22:01:12 -0500

    Initializing benlilley.dev

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var enigma = "<center>Fishers Enigma was a puzzle-solving ARG created by Logan Cover and I to engage our friends during the summer. \n It combined cryptography, computer science, and fun puzzles for teams to try and solve (and be the first to finish).";
+var enigmaLink = "<br><br><a href = 'https://fishersenigma.com'>Fishers Enigma</a></center>";
+enigma += enigmaLink;
+var sched = "<center>SchedLink is an efficient and easy way to check when class periods start and end at Hamilton Southeastern schools. The website and app were developed by the SchedLink team and I throughout my junior and senior year of high school.";
+var schedLink = "<br><br><a href = 'https://schedlink.com'>SchedLink</a></center>";
+sched += schedLink;
+var chem = "<center>During my junior year of high school, I developed a JSON/SQL based database for managing the science department's chemical storage. This database utilized front-end and back-end design using Javascript, jQuery, PHP, SQL, and JSON.";
+function visible(content){
+  document.getElementById("placeholder").innerHTML = content;
+  document.getElementById("placeholder").style.visibility = "visible";
+  $("#placeholder").fadeIn();
+}
